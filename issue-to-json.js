8c2008b1de4765786ad08f21b8552888199017d6
Zach Leatherman <zachleatherman@gmail.com>
2022-03-18 14:40:44 -0500

    Use inputs for folder and issue-template file

--- issue-to-json.js
+++ issue-to-json.js
@@ -1,7 +1,8 @@
 import { getInput, exportVariable, setFailed } from "@actions/core";
 import * as github from "@actions/github";
-import { writeFile, mkdir } from "fs/promises";
+import { writeFile, mkdir, readFile } from "fs/promises";
 import { createHash } from "crypto";
+import yaml from "js-yaml";
 import path from "node:path";
 
 function getFileName(url) {
@@ -11,32 +12,39 @@ function getFileName(url) {
   return hash.digest("base64url").substr(0, 10) + ".json";
 }
 
-function parseIssueBody(body) {
-  let [urlTitle, url, sourceTitle, source_url, authorsTitle, authors] = body.split("\n").filter(entry => !!entry);
+function parseIssueBody(githubFormData, body) {
+  let fields = githubFormData.body;
 
-  if(source_url === "_No response_") {
-    source_url = "";
-  }
-
-  if(authors === "_No response_") {
-    authors = "";
-  }
-
-  let authorsList = authors.split(" ").map(entry => {
-    return entry.trim().replace(/\,/g, "");
-  }).filter(entry => !!entry).map(entry => {
-    return entry.startsWith("@") ? entry : `@${entry}`;
+  let bodyData = body.split("\n").filter(entry => {
+    return !!entry && !entry.startsWith("###")
+  }).map(entry => {
+    return entry === "_No response_" ? "" : entry;
   });
 
-  return {
-    url,
-    source_url,
-    authors: authorsList
+  console.log( {fields, bodyData} );
+  let returnObject = {};
+  for(let j = 0, k = bodyData.length; j<k; j++) {
+    if(!fields[j]) {
+      continue;
+    }
+
+    let entry = bodyData[j];
+    // Clean up GitHub usernames
+    if(fields[j] && fields[j].attributes && fields[j].attributes.label && fields[j].attributes.label.endsWith("(GitHub usernames)")) {
+      entry = entry.split(" ").map(name => {
+        return name.trim().replace(/\,/g, "");
+      }).filter(name => !!name).map(name => {
+        return name.startsWith("@") ? name : `@${name}`;
+      });
+    }
+
+    returnObject[fields[j].id] = entry;
   }
+  console.log( { returnObject } );
+  return returnObject;
 }
 
 function getTestData() {
-      
   return {
     number: 1,
     title: "I built something",
@@ -50,27 +58,33 @@ https://github.com/zachleat/zachleat.com
 
 ### Authors (GitHub usernames)
 
-_No response_`,
+@zachleat, @pdehaan`,
   };
 }
 
 export async function issueToJson() {
   try {
-    const outputDir = getInput("dataFolderPath");
-
+    const outputDir = getInput("folder");
+    
     if (!github.context.payload.issue) {
       setFailed("Cannot find GitHub issue");
       return;
     }
 
+    const issueTemplateFile = getInput("issue-template");
+    const issueTemplate = await readFile(path.join("./github/ISSUE_TEMPLATE/", issueTemplateFile), "utf8");
+    // const issueTemplate = await readFile("./test/sample-issue-template.yml");
+
     const { title, number, body } = github.context.payload.issue;
     // const { title, number, body } = getTestData();
+
+    const formData = yaml.load(issueTemplate);
 
     if (!title || !body) {
       throw new Error("Unable to parse GitHub issue.");
     }
 
-    const issueData = parseIssueBody(body);
+    const issueData = parseIssueBody(formData, body);
 
     exportVariable("IssueNumber", number);
 
